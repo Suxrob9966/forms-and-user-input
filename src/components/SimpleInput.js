826e7f95f3bbb867a67f9ba6e5d25a85dbf457ad
Suxrob9966 <rabbimovich9966@gmail.com>
2022-09-11 14:21:15 -0500

    getting user input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,36 @@
+import { useState, useRef } from "react";
+// 2 approaches of showing the submitted form values: 1. with state 2. with ref
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enteredName); // with state
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue); // with ref
+
+    nameInputRef.current.value = ""; // => not ideal, DO NOT MANIPULATE DOM ELEMENT
+   // setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
