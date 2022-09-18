673d2a6cf1d99098a8974640cf2df0c2bd95a646
Suxrob9966 <rabbimovich9966@gmail.com>
2022-09-18 14:26:02 -0500

    adding name input for basic form

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,10 +1,43 @@
+import { useState } from "react";
+
 const BasicForm = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [nameIsTouched, setNameIsTouched] = useState(false);
+  const nameIsValid = enteredName.trim() !== "";
+  const nameIsInvalid = !nameIsValid && nameIsTouched;
+
+  const nameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const nameBlurHandler = (event) => {
+    setNameIsTouched(true);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    setEnteredName("");
+    setNameIsTouched(false);
+  };
+
+  const nameInputClasses = nameIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className="control-group">
-        <div className="form-control">
+        <div className={nameInputClasses}>
           <label htmlFor="name">First Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            onChange={nameChangeHandler}
+            onBlur={nameBlurHandler}
+            value={enteredName}
+          />
+          {nameIsInvalid && <p className="error-text">Enter name!</p>}
         </div>
         <div className="form-control">
           <label htmlFor="name">Last Name</label>
